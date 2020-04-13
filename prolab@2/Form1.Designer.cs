f1575d119cef3b10bd19ea8c08725ff1522fb8f0
enes-kesen <52306218+enes-kesen@users.noreply.github.com>
2020-04-13 14:28:33 +0300

    Proje dosyası ekle.

---
+++ prolab@2/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace prolab_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Orange;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(1449, 88);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 63);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Rotayi Hesapla";
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(965, 60);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(387, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(1004, 86);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(318, 69);
+            this.listBox1.TabIndex = 5;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // listBox2
+            // 
+            this.listBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 25;
+            this.listBox2.Location = new System.Drawing.Point(568, 22);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(270, 129);
+            this.listBox2.TabIndex = 7;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox2.Image = global::prolab_2.Properties.Resources.postman_PNG8;
+            this.pictureBox2.Location = new System.Drawing.Point(374, 258);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(116, 137);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click_2);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::prolab_2.Properties.Resources.Türkiye;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1824, 1002);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ prolab@2/sehirlerin_kodlari.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prolab_2
+{
+
+
+    public class sehirlerin_kodlari
+    {
+        private int kod;
+
+        public sehirlerin_kodlari(string city)
+        {
+            switch (city)
+            {
+                case "adana":
+                    kod = 0;
+                    break;
+                case "adıyaman":
+                    kod = 1;
+                    break;
+                case "afonkarahisar":
+                    kod = 2;
+                    break;
+                case "ağrı":
+                    kod = 3;
+                    break;
+                case "amasya":
+                    kod = 4;
+                    break;
+                case "ankara":
+                    kod = 5;
+                    break;
+                case "antalya":
+                    kod = 6;
+                    break;
+                case "artvin":
+                    kod = 7;
+                    break;
+                case "aydın":
+                    kod = 8;
+                    break;
+                case "balıkesir":
+                    kod = 9;
+                    break;
+                case "bilecik":
+                    kod = 10;
+                    break;
+                case "bingöl":
+                    kod = 10;
+                    break;
+                case "bitlis":
+                    kod = 12;
+                    break;
+                case "bolu":
+                    kod = 13;
+                    break;
+                case "burdur":
+                    kod = 14;
+                    break;
+                case "bursa":
+                    kod = 15;
+                    break;
+                case "çanakkale":
+                    kod = 16;
+                    break;
+                case "çankırı":
+                    kod = 17;
+                    break;
+                case "çorum":
+                    kod = 18;
+                    break;
+                case "denizli":
+                    kod = 19;
+                    break;
+                case "diyarbakır":
+                    kod = 20;
+                    break;
+                case "edirne":
+                    kod = 21;
+                    break;
+                case "elazığ":
+                    kod = 22;
+                    break;
+                case "erzincan":
+                    kod = 23;
+                    break;
+                case "erzurum":
+                    kod = 24;
+                    break;
+                case "eskişehir":
+                    kod = 25;
+                    break;
+                case "gaziantep":
+                    kod = 26;
+                    break;
+                case "giresun":
+                    kod = 27;
+                    break;
+                case "gümüşhane":
+                    kod = 28;
+                    break;
+                case "hakkari":
+                    kod = 29;
+                    break;
+                case "hatay":
+                    kod = 30;
+                    break;
+                case "ısparta":
+                    kod = 31;
+                    break;
+                case "mersin":
+                    kod = 32;
+                    break;
+                case "istanbul":
+                    kod = 33;
+                    break;
+                case "izmir":
+                    kod = 34;
+                    break;
+                case "kars":
+                    kod = 35;
+                    break;
+                case "kastamanu":
+                    kod = 36;
+                    break;
+                case "kayseri":
+                    kod = 37;
+                    break;
+                case "kırklareli":
+                    kod = 38;
+                    break;
+                case "kırşehir":
+                    kod = 39;
+                    break;
+                case "kocaeli":
+                    kod = 40;
+                    break;
+                case "konya":
+                    kod = 41;
+                    break;
+                case "kütahya":
+                    kod = 42;
+                    break;
+                case "malatya":
+                    kod = 43;
+                    break;
+                case "manisa":
+                    kod = 44;
+                    break;
+                case "kahramanmaraş":
+                    kod = 45;
+                    break;
+                case "mardin":
+                    kod = 46;
+                    break;
+                case "muğla":
+                    kod = 47;
+                    break;
+                case "muş":
+                    kod = 48;
+                    break;
+                case "nevşehir":
+                    kod = 49;
+                    break;
+                case "niğde":
+                    kod = 50;
+                    break;
+                case "ordu":
+                    kod = 51;
+                    break;
+                case "rize":
+                    kod = 52;
+                    break;
+                case "sakarya":
+                    kod = 53;
+                    break;
+                case "samsun":
+                    kod = 54;
+                    break;
+                case "siirt":
+                    kod = 55;
+                    break;
+                case "sinop":
+                    kod = 56;
+                    break;
+                case "sivas":
+                    kod = 57;
+                    break;
+                case "tekirdağ":
+                    kod = 58;
+                    break;
+                case "tokat":
+                    kod = 59;
+                    break;
+                case "trabzon":
+                    kod = 60;
+                    break;
+                case "tunceli":
+                    kod = 61;
+                    break;
+                case "şanlıurfa":
+                    kod = 62;
+                    break;
+                case "uşak":
+                    kod = 63;
+                    break;
+                case "van":
+                    kod = 64;
+                    break;
+                case "yozgat":
+                    kod = 65;
+                    break;
+                case "zonguldak":
+                    kod = 66;
+                    break;
+                case "aksaray":
+                    kod = 67;
+                    break;
+                case "barbut":
+                    kod = 68;
+                    break;
+                case "karaman":
+                    kod = 69;
+                    break;
+                case "kırıkkale":
+                    kod = 70;
+                    break;
+                case "batman":
+                    kod = 71;
+                    break;
+                case "şırnak":
+                    kod = 72;
+                    break;
+                case "bartın":
+                    kod = 73;
+                    break;
+                case "ardahan":
+                    kod = 74;
+                    break;
+                case "ığdır":
+                    kod = 75;
+                    break;
+                case "yalova":
+                    kod = 76;
+                    break;
+                case "karabük":
+                    kod = 77;
+                    break;
+                case "kilis":
+                    kod = 78;
+                    break;
+                case "osmaniye":
+                    kod = 79;
+                    break;
+                case "düzce":
+                    kod = 80;
+                    break;
+            }
+        }
+        public int plaka
+        {
+            get { return kod; }
+            set { kod = value; }
+        }
+    }
+}
+
+ 
